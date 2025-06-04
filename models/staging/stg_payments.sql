5b5a0618a613232d0d6e6503600c330ebb119d1b
mossit2025 <mossit2025@gmail.com>
2025-06-04 21:03:52 +0200

    add status

---
+++ models/staging/stg_payments.sql
@@ -0,0 +1,17 @@
+{{ 
+  config(
+    pre_hook="USE ROLE {{ env_var('DBT_ENV_SNOWFLAKE_ROLE') }}"
+  )
+}}
+
+
+
+select
+    id as customer_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+    amount,
+    created as created_at,
+    _batched_at as update_timestamp
+from {{ source('raw_jaffle_shop', 'customers') }}
